303432addeb1883e6d4864c87c857f9e629525fa
David Hartman <djhartman1216@gmail.com>
2020-01-22 17:40:27 -0500

    Started project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MOCARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
